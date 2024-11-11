68caef6bbc2e8b4d711f807ba06a3938137ef0f3
sena <selina.natschke@t-systems.com>
2024-11-11 12:45:57 +0100

    create suite and second test class without consoleCaptor

---
+++ src/test/java/eu/tsystems/mms/tic/testerra/plugins/teamcity/test/SimpleTest2.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *       Martin Großmann
+ */
+
+package eu.tsystems.mms.tic.testerra.plugins.teamcity.test;
+
+import eu.tsystems.mms.tic.testframework.annotations.Fails;
+import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
+import eu.tsystems.mms.tic.testframework.utils.TimerUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Simple Tests for validating teamcity progess pushers
+ * <p>
+ * This tests should results as follows:
+ * <p>
+ * *** Stats: Test Methods Count: 8 (7 relevant)
+ * *** Stats: Failed: 1
+ * *** Stats: Retried: 1
+ * *** Stats: Expected Failed: 1
+ * *** Stats: Skipped: 1
+ * *** Stats: Passed: 4 ? Recovered: 1
+ *
+ * @author Martin Großmann
+ */
+public class SimpleTest2 extends TesterraTest {
+
+    private static final int TEST_DURATION = 6_000;
+
+    @Test(priority = 1)
+    public void testT01_SimplePassedTest() {
+        TimerUtils.sleep(TEST_DURATION);
+        Assert.assertTrue(true);
+    }
+
+    @Test(priority = 2)
+    public void testT02_SimplePassedTest() {
+        TimerUtils.sleep(TEST_DURATION);
+        Assert.assertTrue(true);
+    }
+
+    @Test(priority = 3)
+    public void testT03_SimpleFailedTest() {
+        TimerUtils.sleep(TEST_DURATION);
+        Assert.fail("Failing for reasons");
+    }
+
+    @Test(dependsOnMethods = {"testT03_SimpleFailedTest"}, priority = 4)
+    public void testT04_SkippedTest() {
+        TimerUtils.sleep(TEST_DURATION);
+        Assert.assertTrue(true);
+    }
+
+    @Test(priority = 5)
+    @Fails(ticketString = "ticket1", description = "Failing for reasons")
+    public void testT05_SimpleExpectedFailedTest() {
+        TimerUtils.sleep(TEST_DURATION);
+        Assert.fail("Failing for reasons...");
+    }
+
+    AtomicInteger counter = new AtomicInteger(0);
+
+    @Test(priority = 6)
+    public void testT06_RetriedTestSecondRunPassed() {
+        this.counter.incrementAndGet();
+        if (counter.get() == 1) {
+            // Message is already defined in test.properties
+            Assert.assertTrue(false, "testT05_RetriedTestSecondRunPassed");
+        } else {
+            Assert.assertTrue(true);
+        }
+
+    }
+}
